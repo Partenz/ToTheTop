f33affa6daedc3225e8411c077e8c81f82540261
황상준 <hsj45935@gmail.com>
2025-10-27 00:13:28 +0900

    플레이어 투명 배경으로 변경

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from pico2d import *
 open_canvas()
 player = load_image('player_idle.png')
 
+width = 0
+height = 256
 while True:
     clear_canvas()
-    player.clip_draw()
+    player.clip_draw(256 * width, height, 256, 256, 400, 300)
     update_canvas()
-    delay(1)
 
+    width = (width + 1) % 4
+    if width == 0 and height == 256:
+        height -= 256
+    elif width == 0 and height == 0:
+        height += 256
+
+    delay(1)
