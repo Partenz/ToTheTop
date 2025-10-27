f50953579c64567b41e560d40307e9520dd28b98
황상준 <hsj45935@gmail.com>
2025-10-27 21:30:49 +0900

    아직 플레이어 만드는 중..

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from pico2d import *
 
 open_canvas()
-player = load_image('player_idle.png')
+player = load_image('playerGPT.png')
 
 width = 0
 height = 256
 while True:
     clear_canvas()
-    player.clip_draw(256 * width, height, 256, 256, 400, 300)
+    player.clip_draw(256 * width, 280 * 1, 256, 300, 400, 300)
     update_canvas()
 
     width = (width + 1) % 4
-    if width == 0 and height == 256:
-        height -= 256
-    elif width == 0 and height == 0:
-        height += 256
 
     delay(1)
