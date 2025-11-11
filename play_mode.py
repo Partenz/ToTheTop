67217dc50c78f61c6f7c2544487b71e4860856d3
황상준 <hsj45935@gmail.com>
2025-11-12 08:22:43 +0900

    플레이 모드와 게임 프레임워크 추가

---
+++ play_mode.py
@@ -0,0 +1,45 @@
+import random
+from pico2d import *
+
+import game_framework
+import game_world
+
+from player import Player
+
+player = None
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            player.handle_event(event)
+
+def init():
+    global player
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+    game_world.add_collision_pair('grass:ball', grass, None)
+
+    player = Player()
+    game_world.add_object(player, 3)
+
+
+def update():
+    game_world.update()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
