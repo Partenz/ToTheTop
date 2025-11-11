795a610c1ae48a8feebe87d30eb42c49b2dcfa2b
황상준 <hsj45935@gmail.com>
2025-11-12 01:42:07 +0900

    상태 머신 추가

--- player.py
+++ player.py
@@ -1,4 +1,15 @@
 from pico2d import load_image
+import game_world
+from state_machine import StateMachine
+
+class Idle:
+    pass
+
+class Walk:
+    pass
+
+class Attack:
+    pass
 
 class Player:
     def __init__(self):
@@ -9,6 +20,15 @@ class Player:
         self.image['Idle'] = [load_image('./resources/player/player_idle%d' %i +'.png') for i in range(1, 2 + 1)]
         self.image['Walk'] = [load_image('./resources/player/player_walk%d' %i +'.png') for i in range(1, 3 + 1)]
         self.image['Attack'] = [load_image('./resources/player/player_attack%d' %i +'.png') for i in range(1, 4 + 1)]
+
+        self.IDLE = Idle(self)
+        self.WALK = Walk(self)
+        self.ATTACK = Attack(self)
+        self.state_machine = StateMachine(self.IDLE, {
+            self.IDLE: {},
+            self.WALK: {},
+            self.ATTACK: {}
+        })
 
     def update(self):
         pass
