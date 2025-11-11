7a261d01b323b6ef2a7bd227de996fab64644da4
황상준 <hsj45935@gmail.com>
2025-11-12 01:38:11 +0900

    게임 월드 모듈 제작

---
+++ game_world.py
@@ -0,0 +1,32 @@
+world = [[] for _ in range(4)]
+
+def add_object(obj, depth = 0):
+    world[depth].append(obj)
+
+def add_objects(objl, depth = 0):
+    world[depth] += objl
+
+
+def update():
+    for layer in world:
+        for obj in layer:
+            obj.update()
+
+
+def render():
+    for layer in world:
+        for obj in layer:
+            obj.draw()
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+    raise ValueError('Cannot delete non existing object')
+
+
+def clear():
+    global world
+
+    objects = [[] for _ in range(4)]
