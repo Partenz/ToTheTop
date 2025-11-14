daa93e335398baeba5530ee848248dce6bbe1b1d
황상준 <hsj45935@gmail.com>
2025-11-14 20:41:05 +0900

    타이틀 모드 추가

---
+++ title_mode.py
@@ -0,0 +1,38 @@
+from pico2d import *
+
+import game_framework
+import play_mode
+
+image = None
+running = True
+
+def init():
+    global image, running
+
+    image = load_image('./resources/Title.png')
+    running = True
+
+def finish():
+    global image
+    del image
+
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+    image.draw(1920//2, 720//2, 1920, 720)
+    update_canvas()
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            game_framework.change_mode(play_mode)
+
+def pause(): pass
+def resume(): pass
