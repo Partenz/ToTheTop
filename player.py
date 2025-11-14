97ecca949f29f4c4ac65ea53878ae7e86422730d
황상준 <hsj45935@gmail.com>
2025-11-14 21:10:02 +0900

    점프 버그 수정 및 공격 추가 중

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from pico2d import load_image, get_time, draw_rectangle
-from sdl2 import SDL_KEYDOWN, SDLK_LEFT, SDL_KEYUP, SDLK_RIGHT, SDLK_SPACE
+from sdl2 import SDL_KEYDOWN, SDLK_LEFT, SDL_KEYUP, SDLK_RIGHT, SDLK_SPACE, SDLK_a
 
 import game_framework
 import game_world
@@ -13,7 +13,7 @@ RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = {'Idle': 12, 'Run': 8}
+FRAMES_PER_ACTION = {'Idle': 12, 'Run': 8, 'Attack' : 8}
 
 pi = 3.141592
 
@@ -41,13 +41,26 @@ class Run:
         self.player = player
 
     def enter(self, event):
-        if left_down(event) or right_up(event):
+        if left_down(event):
             self.player.dir = -1
             self.player.face_dir = -1
-        elif right_down(event) or left_up(event):
+        elif right_down(event):
             self.player.dir = 1
             self.player.face_dir = 1
-
+        elif left_up(event):
+            if self.player.ifPlayerJumped:
+                self.player.ifPlayerJumped = False
+                self.player.state_machine.change_state(self.player.IDLE, event)
+            else:
+                self.player.dir = 1
+                self.player.face_dir = 1
+        elif right_up(event):
+            if self.player.ifPlayerJumped:
+                self.player.ifPlayerJumped = False
+                self.player.state_machine.change_state(self.player.IDLE, event)
+            else:
+                self.player.dir = -1
+                self.player.face_dir = -1
     def exit(self, event):
         pass
 
@@ -71,6 +84,7 @@ class Jump:
         self.prev_state = None  # 이전 상태 저장
 
     def enter(self, event):
+        self.player.ifPlayerJumped = True
         self.jump_velocity = self.initial_jump_speed
         self.ground_y = self.player.y
         # 현재 dir을 점프 방향으로 저장
@@ -94,13 +108,38 @@ class Jump:
         # 착지 체크
         if self.player.y <= self.ground_y:
             self.player.y = self.ground_y
-            self.player.state_machine.handle_state_event(('JUMP_END', None))
+            self.player.state_machine.handle_state_event(('TIME_OUT', None))
 
     def draw(self):
         if self.player.face_dir == 1:
             self.player.image['Run'].clip_draw(int(self.player.frame) * 64, 64, 64, 64, self.player.x, self.player.y, self.player.width, self.player.height)
         elif self.player.face_dir == -1:
             self.player.image['Run'].clip_draw(int(self.player.frame) * 64, 128, 64, 64, self.player.x, self.player.y, self.player.width, self.player.height)
+
+class Attack:
+    def __init__(self, player):
+        self.start_time = None
+        self.player = player
+
+    def enter(self, event):
+        self.start_time = get_time()
+
+    def exit(self, event):
+        pass
+
+    def do(self):
+        self.player.frame = (self.player.frame + FRAMES_PER_ACTION['Attack'] * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION['Attack']
+
+        if get_time() - self.start_time > TIME_PER_ACTION:
+            # 공격 애니메이션이 끝나면 이전 상태로 돌아감
+            self.player.state_machine.handle_state_event(('TIME_OUT', None))
+
+    def draw(self):
+        if self.player.face_dir == 1: # 오른쪽
+            self.player.image['Attack'].clip_draw(int(self.player.frame) * 64, 64, 64, 64, self.player.x, self.player.y, self.player.width, self.player.height)
+        elif self.player.face_dir == -1: # 왼쪽
+            self.player.image['Attack'].clip_draw(int(self.player.frame) * 64, 128, 64, 64, self.player.x, self.player.y, self.player.width, self.player.height)
+
 
 class Player:
     def __init__(self):
@@ -110,16 +149,20 @@ class Player:
         self.dir = 0 # 0 정지 1 오른쪽 -1 왼쪽
         self.width = 256
         self.height = 256
+        self.ifPlayerJumped = False
         self.image = {}
         self.image['Idle'] = load_image('./resources/player/Player_IDLE.png')
         self.image['Run'] = load_image('./resources/player/Player_Run.png')
+        self.image['Attack'] = load_image('./resources/player/Player_Attack.png')
         self.IDLE = Idle(self)
         self.RUN = Run(self)
         self.JUMP = Jump(self)
+        self.ATTACK = Attack(self)
         self.state_machine = StateMachine(self.IDLE, {
-            self.IDLE: {left_down: self.RUN, right_down: self.RUN, space_down: self.JUMP},
+            self.IDLE: {left_down: self.RUN, right_down: self.RUN, left_up: self.RUN, right_up: self.RUN, space_down: self.JUMP, a_down: self.ATTACK},
             self.RUN: {left_down: self.IDLE, right_down: self.IDLE, left_up: self.IDLE, right_up: self.IDLE, space_down: self.JUMP},
-            self.JUMP: {jump_end: self.IDLE}
+            self.JUMP: {time_out: self.IDLE},
+            self.ATTACK: {},
 
         })
 
@@ -151,5 +194,8 @@ def right_up(event):
 def space_down(event):
     return event[0] == 'INPUT' and event[1].type == SDL_KEYDOWN and event[1].key == SDLK_SPACE
 
-def jump_end(event):
-    return event[0] == 'JUMP_END'
+def time_out(event):
+    return event[0] == 'TIME_OUT'
+
+def a_down(event):
+    return event[0] == 'INPUT' and event[1].type == SDL_KEYDOWN and event[1].key == SDLK_a
