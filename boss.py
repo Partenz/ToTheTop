12fe619614955f8354e7f8c97225e8a16cd27a23
황상준 <hsj45935@gmail.com>
2025-12-05 17:15:19 +0900

    BossAttack 클래스 생성

--- boss.py
+++ boss.py
@@ -22,6 +22,31 @@ FRAMES_PER_ACTION = {'Appearance': 8,'Idle': 8, 'Hurt' : 4, 'Death' : 6, 'Attack
 GRAVITY = 9.8  # 중력 가속도 (m/s²)
 GRAVITY_PPS = GRAVITY * PIXEL_PER_METER  # 중력을 픽셀 단위로 변환
 
+class BossAttack:
+    def __init__(self):
+        self.creation_time = get_time()
+        self.x = None
+        self.y = None
+
+    def update(self):
+        # 일정 시간 후 자동 소멸
+        if get_time() - self.creation_time > 0.5: # 예: 0.5초
+            game_world.remove_object(self)
+
+    def get_bb(self):
+        # 무기의 바운딩 박스 반환
+        return self.x - 34, self.y - 50, self.x + 34, self.y + 25
+
+    def draw(self):
+        # 디버깅용으로 바운딩 박스 그리기
+        draw_rectangle(*self.get_bb())
+
+    def handle_collision(self, group, other):
+        if group == 'bossAttack:player':
+            pass
+        elif group == 'bossAttack:weapon'
+            pass
+
 class Boss:
     image = None
 
